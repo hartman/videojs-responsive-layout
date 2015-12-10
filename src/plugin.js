77d37581677d9b54a15ad89e8427700c71879a46
Derk-Jan Hartman <hartman.wiki@gmail.com>
2015-12-10 22:49:14 +0100

    Rewrite the plugin to provide a Layouter instance
    This will help when later we want to make it configurable

--- src/plugin.js
+++ src/plugin.js
@@ -3,33 +3,18 @@ import videojs from 'video.js';
 const debounce = require('throttle-debounce').debounce;
 
 // Default options for the plugin.
-const defaults = {};
-
-/**
- * Function to invoke when the player is ready.
- *
- * This is a great place for your plugin to initialize itself. When this
- * function is called, the player will have its DOM and child components
- * in place.
- *
- * @function onPlayerReady
- * @param    {Player} player
- * @param    {Object} [options={}]
- */
-const onPlayerReady = (player, options) => {
-  player.addClass('vjs-responsive-layout');
-  player.trigger('resize');
-  player.RLwindowListener_ = window.addEventListener('resize', debouncedCheckSize);
+const defaults = {
+  debounceDelay: 400
 };
 
 /**
  * Retrieve the outerWidth of an element, including margins
  *
- * @function outerWidth
+ * @function getElementOuterWidth
  * @param    {Element} el to measure
  * @return   {number} the width of the element in pixels
  */
-const outerWidth = (el) => {
+const getElementOuterWidth = (el) => {
   let width = el.offsetWidth;
   let style = getComputedStyle(el);
 
@@ -40,46 +25,50 @@ const outerWidth = (el) => {
 /**
  * Retrieve the width an element
  *
- * @function width
+ * @function getElementWidth
  * @param    {Element} el to measure
  * @return   {number} the width of the element in pixels
  */
-const width = (el) => {
+const getElementWidth = (el) => {
   return parseInt(getComputedStyle(el).width, 10);
 };
 
 /**
- * Check if an element is currently visible
+ * Check if an element is currently visible.
  *
- * @function isVisible
+ * Use this to filter on elements that should be taken into account during calculations.
+ *
+ * @function isElementVisible
  * @param    {Element} el to test
  * @return   {boolean} true if el is visible
  */
-const isVisible = (el) => {
+const isElementVisible = (el) => {
   return (el.offsetWidth > 0 || el.offsetHeight > 0);
 };
 
 /**
- * Set a layout class on an element
+ * Set a video.js layout class on an element
  *
  * A layout can be: vjs-layout-tiny, vjs-layout-x-small, vjs-layout-small.
  * Anything else will reset back to defaults
  *
- * @function isVisible
- * @param    {Element} el to apply the layout to
- * @param    {string} layout class to set
+ * @function setVideoJsLayout
+ * @param    {Player} player to apply the layout to
+ * @param    {string} layoutClass name of the class to be set
  * @return   {boolean} true if el is visible
  */
-const setLayout = (el, layout) => {
-  if (layout === 'vjs-layout-tiny') {
+const setVideoJsLayout = (player, layoutClass) => {
+  let el = player.el();
+
+  if (layoutClass === 'vjs-layout-tiny') {
     videojs.addClass(el, 'vjs-layout-tiny');
     videojs.removeClass(el, 'vjs-layout-small');
     videojs.removeClass(el, 'vjs-layout-x-small');
-  } else if (layout === 'vjs-layout-x-small') {
+  } else if (layoutClass === 'vjs-layout-x-small') {
     videojs.addClass(el, 'vjs-layout-x-small');
     videojs.removeClass(el, 'vjs-layout-tiny');
     videojs.removeClass(el, 'vjs-layout-small');
-  } else if (layout === 'vjs-layout-small') {
+  } else if (layoutClass === 'vjs-layout-small') {
     videojs.addClass(el, 'vjs-layout-small');
     videojs.removeClass(el, 'vjs-layout-tiny');
     videojs.removeClass(el, 'vjs-layout-x-small');
@@ -89,6 +78,190 @@ const setLayout = (el, layout) => {
     videojs.removeClass(el, 'vjs-layout-small');
   }
 };
+
+const makeBigger = (player) => {
+  let el = player.el();
+
+  if (videojs.hasClass(el, 'vjs-layout-tiny')) {
+    setVideoJsLayout(player, 'vjs-layout-x-small');
+  } else if (videojs.hasClass(el, 'vjs-layout-x-small')) {
+    setVideoJsLayout(player, 'vjs-layout-small');
+  } else if (videojs.hasClass(el, 'vjs-layout-small')) {
+    setVideoJsLayout(player, 'default');
+  } else {
+    return;
+  }
+  player.trigger('resize');
+};
+
+const makeSmaller = (player) => {
+  let el = player.el();
+
+  if (videojs.hasClass(el, 'vjs-layout-tiny')) {
+    return;
+  } else if (videojs.hasClass(el, 'vjs-layout-x-small')) {
+    setVideoJsLayout(player, 'vjs-layout-tiny');
+  } else if (videojs.hasClass(el, 'vjs-layout-small')) {
+    setVideoJsLayout(player, 'vjs-layout-x-small');
+  } else {
+    setVideoJsLayout(player, 'vjs-layout-small');
+  }
+  player.trigger('resize');
+};
+
+const updateVideoJsLayout = (layouter, playerWidth, controlBarWidth, controlWidth) => {
+  let progressWidth = getElementOuterWidth(
+    layouter.el.querySelectorAll('.vjs-progress-control')[0]
+  );
+
+  if (controlBarWidth > playerWidth) {
+    makeSmaller(layouter.player);
+  } else if ((videojs.hasClass(layouter.el, 'vjs-layout-x-small') ||
+               videojs.hasClass(layouter.el, 'vjs-layout-small')) &&
+               // progressbar more that twice it's default size
+               progressWidth > controlWidth * 2
+  ) {
+    makeBigger(layouter.player);
+  } else if (playerWidth > controlBarWidth + controlWidth &&
+             !videojs.hasClass(layouter.el, 'vjs-layout-x-small') &&
+             !videojs.hasClass(layouter.el, 'vjs-layout-small')
+  ) {
+    makeBigger(layouter.player);
+  }
+};
+
+const dimensionsCheck = function() {
+  if (!this.el) {
+    return;
+  }
+  let playerWidth = this.getPlayerWidth();
+  let controlWidth = this.getControlWidth();
+  let controlBarWidth = this.getControlBarWidth();
+
+  if (this.options.updateVideoJsLayout) {
+    this.options.updateVideoJsLayout(this, playerWidth, controlBarWidth, controlWidth);
+  } else {
+    updateVideoJsLayout(this, playerWidth, controlBarWidth, controlWidth);
+  }
+};
+
+class Layouter {
+  constructor(player, options) {
+    this.player_ = player;
+    this.options_ = options;
+    this.debouncedCheckSize_ = debounce(options.debounceDelay, dimensionsCheck);
+  }
+
+  ready() {
+    this.player.addClass('vjs-responsive-layout');
+    this.windowResizeListener_ = window.addEventListener(
+      'resize',
+      () => this.debouncedCheckSize_()
+    );
+
+    this.player.on(['play', 'resize'], () => this.debouncedCheckSize_());
+    this.player.on('dispose', function() {
+      window.removeEventListener(this.windowResizeListener_);
+    });
+
+    // Let's do the first measure
+    this.player.trigger('resize');
+  }
+
+  /**
+   * Retrieve player to which this Layouter object belongs
+   *
+   * @property player
+   * @return   {number} the width of the controlbar in pixels
+   */
+  get player() {
+    return this.player_;
+  }
+
+  get el() {
+    return this.player_.el();
+  }
+
+  get options() {
+    return this.options_;
+  }
+
+  /**
+   * Retrieve current width of a control in the video.js controlbar
+   *
+   * This function relies on the presence of the play control. If you
+   * mess with it's visibility, things likely will break :)
+   *
+   * @function getControlBarWidth
+   * @return   {number} the width of the controlbar in pixels
+   */
+  getControlWidth() {
+    return getElementOuterWidth(this.el.querySelectorAll('.vjs-play-control')[0]);
+  }
+
+  /**
+   * Retrieve current width of the video.js controlbar
+   *
+   * @function getControlBarWidth
+   * @return   {number} the width of the controlbar in pixels
+   */
+  getControlBarWidth() {
+    let controlBarWidth = 0;
+    let cbElements = this.el.querySelectorAll('.vjs-control-bar > *');
+
+    Array.from(cbElements).forEach(function(el) {
+      if (isElementVisible(el)) {
+        controlBarWidth += getElementOuterWidth(el);
+      }
+    });
+    return controlBarWidth;
+  }
+
+  /**
+   * Retrieve current width of the video.js player element
+   *
+   * @function getPlayerWidth
+   * @return   {number} the width of the player in pixels
+   */
+  getPlayerWidth() {
+    return getElementWidth(this.el);
+  }
+
+  /**
+   * Retrieve the outerWidth of an element, including margins
+   *
+   * @function outerWidth
+   * @param    {Element} el to measure
+   * @return   {number} the width of the element in pixels
+   */
+  static getElementOuterWidth(el) {
+    return getElementOuterWidth(el);
+  }
+
+  /**
+   * Retrieve the width an element
+   *
+   * @function getElementWidth
+   * @param    {Element} el to measure
+   * @return   {number} the width of the element in pixels
+   */
+  static getElementWidth(el) {
+    return getElementWidth(el);
+  }
+
+  /**
+   * Check if an element is currently visible.
+   *
+   * Use this to filter on elements that should be taken into account during calculations.
+   *
+   * @function isElementVisible
+   * @param    {Element} el to test
+   * @return   {boolean} true if el is visible
+   */
+  static isElementVisible(el) {
+    return isElementVisible(el);
+  }
+}
 
 /**
  * A video.js plugin.
@@ -103,77 +276,10 @@ const setLayout = (el, layout) => {
  *           An object of options left to the plugin author to define.
  */
 const responsiveLayout = function(options) {
-  let player = this;
-  let el_ = player.el();
-
-  const makeBigger = () => {
-    if (videojs.hasClass(el_, 'vjs-layout-tiny')) {
-      setLayout(el_, 'vjs-layout-x-small');
-    } else if (videojs.hasClass(el_, 'vjs-layout-x-small')) {
-      setLayout(el_, 'vjs-layout-small');
-    } else if (videojs.hasClass(el_, 'vjs-layout-small')) {
-      setLayout(el_, 'default');
-    } else {
-      return;
-    }
-    player.trigger('resize');
-  };
-  const makeSmaller = () => {
-    if (videojs.hasClass(el_, 'vjs-layout-tiny')) {
-      return;
-    } else if (videojs.hasClass(el_, 'vjs-layout-x-small')) {
-      setLayout(el_, 'vjs-layout-tiny');
-    } else if (videojs.hasClass(el_, 'vjs-layout-small')) {
-      setLayout(el_, 'vjs-layout-x-small');
-    } else {
-      setLayout(el_, 'vjs-layout-small');
-    }
-    player.trigger('resize');
-  };
-
-  const checkSize = () => {
-    if (!el_) {
-      return;
-    }
-    let playerWidth = width(el_);
-    let controlWidth = outerWidth(el_.querySelectorAll('.vjs-play-control')[0]);
-    let controlBarWidth = 0;
-    let progressWidth = outerWidth(el_.querySelectorAll('.vjs-progress-control')[0]);
-
-    let cbElements = el_.querySelectorAll('.vjs-control-bar > *');
-    Array.from(cbElements).forEach(function(el) {
-      if (isVisible(el)) {
-        controlBarWidth += outerWidth(el);
-      }
-    });
-
-    if (controlBarWidth > playerWidth) {
-      makeSmaller();
-    } else if ((videojs.hasClass(el_, 'vjs-layout-x-small') ||
-                 videojs.hasClass(el_, 'vjs-layout-small')) &&
-                 // progressbar more that twice it's default size
-                 progressWidth > controlWidth * 2
-    ) {
-      makeBigger();
-    } else if (playerWidth > controlBarWidth + controlWidth &&
-               !videojs.hasClass(el_, 'vjs-layout-x-small') &&
-               !videojs.hasClass(el_, 'vjs-layout-small')
-    ) {
-      makeBigger();
-    }
-  };
-  const debouncedCheckSize = debounce(200, checkSize);
-
-  player.on(['play', 'resize'], debouncedCheckSize);
-  player.on('dispose', function() {
-    window.removeEventListener(player.RLwindowListener_);
-  });
+  let layout = new Layouter(this, videojs.mergeOptions(defaults, options));
 
   this.ready(() => {
-    let options = videojs.mergeOptions(defaults, options);
-    player.addClass('vjs-responsive-layout');
-    player.trigger('resize');
-    player.RLwindowListener_ = window.addEventListener('resize', debouncedCheckSize);
+    layout.ready();
   });
 };
 
